291d473f654f74fb9633876998db62bdea7f40d8
mas-sato <mas.sat.968@gmail.com>
2015-11-04 03:22:46 +0900

    4th Commit

--- 1.php
+++ 1.php
@@ -6,7 +6,16 @@ $output1 = "hello";
 $output2 = "world";
 echo $output1 ." ". $output2 ."\n \n";
 
+
 $rand_num = rand(1,6);
-echo "さいころの値は".$rand_num."です。\n";
+echo "さいころの値は".$rand_num."です。\n \n";
+
+echo "値段計算→\n";
+$apple = 350;
+$apple_num = rand(1,10);
+echo "りんごの値段：".$apple."円\n";
+echo "リンゴを買う数：".$apple_num."個\n";
+$total = $apple * $apple_num;
+echo "合計".$total."円です。\n \n";
 
 ?>
